37e21aa91f6f017b0306a4eb259f5d6a2f9ea8ab
bsarpdag23 <165482516+bsarpdag23@users.noreply.github.com>
2025-05-02 20:50:43 +0300

    değişkenler atandı

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pygame
 import random
 
 from objects import Bar, Dot, Player, Message, Particle, ScoreCard, Button
+
+pygame.init()
+SCREEN = WIDTH, HEIGHT = 288, 512
+
+info = pygame.display.Info()
+width = info.current_w
+height = info.current_h
