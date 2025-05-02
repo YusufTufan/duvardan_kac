c2986ce52dbfdf92b8f16fe3556a39c67b6ce2ef
Arin0619 <153939423+Arin0619@users.noreply.github.com>
2025-05-02 20:25:03 +0300

    Update objects.py

--- objects.py
+++ objects.py
@@ -15,7 +15,14 @@ flip_fx = pygame.mixer.Sound('Sounds/flip.mp3')  # Yön değiştirince çıkan s
 # Oyuncuyu temsil eden sınıf
 class Player:
 	def __init__(self, win):
-		
+		self.win = win  # Çizim yapılacak yüzey
+		self.image = pygame.image.load('Assets/rect.png')  # Oyuncunun görseli
+		self.image = pygame.transform.scale(self.image, (16,16))  # Küçültüyoruz
+		self.reset()  # Başlangıç pozisyonu
+
+		self.dy = 4  # Y eksenindeki hareket miktarı
+		self.frame_top = HEIGHT//2 - 75  # Üst sınır
+		self.frame_bottom = HEIGHT//2 + 75  # Alt sınır
 	# Oyuncuyu ekranda güncelleyen fonksiyon
 	def update(self, show_player, clicked):
 		
