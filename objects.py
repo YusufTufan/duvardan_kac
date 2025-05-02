487111e8e16166ef44e419e1df3559fbbe50821f
Arin0619 <153939423+Arin0619@users.noreply.github.com>
2025-05-02 20:23:44 +0300

    Update objects.py

--- objects.py
+++ objects.py
@@ -11,3 +11,15 @@ pygame.mixer.init()
 # Ses efektleri
 dash_fx = pygame.mixer.Sound('Sounds/dash.mp3')  # Duvara çarpınca çıkan ses
 flip_fx = pygame.mixer.Sound('Sounds/flip.mp3')  # Yön değiştirince çıkan ses
+
+# Oyuncuyu temsil eden sınıf
+class Player:
+	def __init__(self, win):
+		
+	# Oyuncuyu ekranda güncelleyen fonksiyon
+	def update(self, show_player, clicked):
+		
+
+	# Oyuncuyu başlangıç konumuna getir
+	def reset(self):
+	
