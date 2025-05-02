51713f27733909a7732ec5c583bd37639979493f
Yusuf Tufan <122537509+YusufTufan@users.noreply.github.com>
2025-05-02 20:52:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ SCREEN = WIDTH, HEIGHT = 288, 512
 info = pygame.display.Info()
 width = info.current_w
 height = info.current_h
+
+if width >= height:
+    win = pygame.display.set_mode(SCREEN, pygame.NOFRAME)
+else:
+    win = pygame.display.set_mode(SCREEN, pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
+
+clock = pygame.time.Clock()
+FPS = 30
