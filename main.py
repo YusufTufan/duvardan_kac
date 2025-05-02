212dcb90aa7e985844504059668a3f98848c2b59
Yusuf Tufan <122537509+YusufTufan@users.noreply.github.com>
2025-05-02 20:53:21 +0300

    Arka planlar yükleniyor

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ else:
 
 clock = pygame.time.Clock()
 FPS = 30
+
+# Arka planlar yükleniyor
+bg = pygame.image.load('Assets/bg4.jpg')
+bg = pygame.transform.scale(bg, SCREEN)
+
+frame_height = 150
+frame = pygame.image.load('Assets/bg3.jpg')
+frame = pygame.transform.scale(frame, (WIDTH - 10, frame_height))
+frameX, frameY = 5, HEIGHT // 2 - frame_height // 2
