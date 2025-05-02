6dbaf103a084afa782a43f6c64f064e7b1500c6b
Yusuf Tufan <122537509+YusufTufan@users.noreply.github.com>
2025-05-02 20:20:02 +0300

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,4 +1,9 @@
 import pygame
 import random
 
+# Ekran boyutunu tanımlıyoruz (ana dosyadaki ile aynı)
 SCREEN = WIDTH, HEIGHT = 288, 512
+
+# Pygame için gerekli modülleri başlatıyoruz
+pygame.font.init()
+pygame.mixer.init()
